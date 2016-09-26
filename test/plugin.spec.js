ba2c1e4219a13d9a3f619384442159b6692cef86
Robin van der Vleuten <robin@webstronauts.co>
2016-09-26 14:25:50 +0200

    Created base specs

---
+++ test/plugin.spec.js
@@ -0,0 +1,11 @@
+const createPersistedState = require('../dist/vuex-persistedstate')
+
+describe('vuex-persistedstate', function () {
+  it('replaces store\'s state when initializing', function () {
+    console.log(createPersistedState)
+  })
+
+  it('subscribes to store\'s state changes when initializing', function () {
+    console.log(createPersistedState)
+  })
+})
