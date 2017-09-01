81bf4cfe81487d0113b45fb858fc63ad9e654923
Robin van der Vleuten <robin@webstronauts.co>
2017-09-01 09:39:17 +0200

    Small refactoring of the build process through rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+const buble = require('rollup-plugin-buble')
+const pkg = require('./package.json')
+
+const banner =
+  `/**
+    * @license
+    *
+    * vuex-persistedstate v${pkg.version}
+    *
+    * (c) ${new Date().getFullYear()} Robin van der Vleuten <robin@webstronauts.co>
+    *
+    * For the full copyright and license information, please view the LICENSE
+    * file that was distributed with this source code.
+    */`
+
+module.exports = {
+  entry: 'src/plugin.js',
+  moduleName: 'createPersistedState',
+  dest: pkg.main,
+  useStrict: false,
+  format: 'umd',
+  globals: {
+    'lodash.merge': 'merge',
+    'object-path': 'objectPath'
+  },
+  external: ['lodash.merge', 'object-path'],
+  plugins: [buble()],
+  banner
+}
