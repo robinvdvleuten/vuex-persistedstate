065df235fd00318a725492de00602fcb436d7650
Robin van der Vleuten <robin@webstronauts.co>
2017-11-21 15:41:47 +0100

    Updated TS typings

--- vuex-persistedstate.d.ts
+++ vuex-persistedstate.d.ts
@@ -1,14 +1,14 @@
 import { Store, MutationPayload } from 'vuex';
 
-export default function createPersistedState(options?: Options): any;
-
 interface Options {
-    storage?: Storage;
     key?: string;
     paths?: string[];
     reducer?: (state: any, paths: string[]) => object;
     subscriber?: (store: typeof Store) => (handler: () => void) => void;
+    storage?: Storage;
     getState?: (key: string, storage: Storage) => any;
     setState?: (key: string, state: typeof Store, storage: Storage) => void;
     filter?: (mutation: MutationPayload) => boolean;
 }
+
+export default function createPersistedState(options?: Options): any;
