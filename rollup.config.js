b553d05999d05dce7d6bc160574de40251027311
Robin van der Vleuten <robin@webstronauts.co>
2017-11-22 10:03:09 +0100

    Also generate sourcemaps per output file

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,22 @@
+import fs from 'fs';
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+
 export default {
+	input: './index.js',
+	useStrict: false,
+	sourcemap: true,
 	plugins: [
 		resolve({
 			jsnext: true,
       main: true
 		}),
 		commonjs()
+	],
+	output: [
+		{ file: pkg.main, format: 'cjs' },
+		{ file: pkg['umd:main'], format: 'umd', name: 'createPersistedState' }
 	]
 };
