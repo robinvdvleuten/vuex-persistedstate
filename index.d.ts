d3ad47ab6b1128b219e6b27d4987d95dcb2943f6
Nelson Liu <nelsliu9121@gmail.com>
2019-07-04 18:33:07 +0800

    ✨ Add rehydrated callback
     #166

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,22 @@
-import { Store, MutationPayload } from 'vuex';
+import { Store, MutationPayload } from 'vuex'
 
 interface Storage {
-    getItem: (key: string) => void;
-    setItem: (key: string, value: any) => void;
-    removeItem: (key: string) => void;
+  getItem: (key: string) => void
+  setItem: (key: string, value: any) => void
+  removeItem: (key: string) => void
 }
 
 interface Options {
-    key?: string;
-    paths?: string[];
-    reducer?: (state: any, paths: string[]) => object;
-    subscriber?: (store: typeof Store) => (handler: () => void) => void;
-    storage?: Storage;
-    getState?: (key: string, storage: Storage) => any;
-    setState?: (key: string, state: typeof Store, storage: Storage) => void;
-    filter?: (mutation: MutationPayload) => boolean;
-    arrayMerger?: (state: any, saved: any) => any;
+  key?: string
+  paths?: string[]
+  reducer?: (state: any, paths: string[]) => object
+  subscriber?: (store: typeof Store) => (handler: () => void) => void
+  storage?: Storage
+  getState?: (key: string, storage: Storage) => any
+  setState?: (key: string, state: typeof Store, storage: Storage) => void
+  filter?: (mutation: MutationPayload) => boolean
+  arrayMerger?: (state: any, saved: any) => any
+  rehydrated?: (store: typeof Store) => void
 }
 
-export default function createPersistedState(options?: Options): any;
+export default function createPersistedState(options?: Options): any
--- index.js
+++ index.js
@@ -1,75 +1,82 @@
-import merge from 'deepmerge';
-import * as shvl from 'shvl';
+import merge from 'deepmerge'
+import * as shvl from 'shvl'
 
 export default function(options, storage, key) {
-  options = options || {};
-  storage = options.storage || (window && window.localStorage);
-  key = options.key || 'vuex';
+  options = options || {}
+  storage = options.storage || (window && window.localStorage)
+  key = options.key || 'vuex'
 
   function canWriteStorage(storage) {
     try {
-      storage.setItem('@@', 1);
-      storage.removeItem('@@');
-      return true;
+      storage.setItem('@@', 1)
+      storage.removeItem('@@')
+      return true
     } catch (e) {}
 
-    return false;
+    return false
   }
 
   function getState(key, storage, value) {
     try {
       return (value = storage.getItem(key)) && typeof value !== 'undefined'
         ? JSON.parse(value)
-        : undefined;
+        : undefined
     } catch (err) {}
 
-    return undefined;
+    return undefined
   }
 
   function filter() {
-    return true;
+    return true
   }
 
   function setState(key, state, storage) {
-    return storage.setItem(key, JSON.stringify(state));
+    return storage.setItem(key, JSON.stringify(state))
   }
 
   function reducer(state, paths) {
     return paths.length === 0
       ? state
       : paths.reduce(function(substate, path) {
-          return shvl.set(substate, path, shvl.get(state, path));
-        }, {});
+          return shvl.set(substate, path, shvl.get(state, path))
+        }, {})
   }
 
   function subscriber(store) {
     return function(handler) {
-      return store.subscribe(handler);
-    };
+      return store.subscribe(handler)
+    }
   }
 
   if (!canWriteStorage(storage)) {
-    throw new Error('Invalid storage instance given');
+    throw new Error('Invalid storage instance given')
   }
 
   return function(store) {
-    const savedState = shvl.get(options, 'getState', getState)(key, storage);
+    const savedState = shvl.get(options, 'getState', getState)(key, storage)
 
     if (typeof savedState === 'object' && savedState !== null) {
-      store.replaceState(merge(store.state, savedState, {
-        arrayMerge: options.arrayMerger || function (store, saved) { return saved },
-        clone: false,
-      }));
+      store.replaceState(
+        merge(store.state, savedState, {
+          arrayMerge:
+            options.arrayMerger ||
+            function(store, saved) {
+              return saved
+            },
+          clone: false,
+        })
+      )
+      ;(options.rehydrated || function() {})(store)
     }
 
-    (options.subscriber || subscriber)(store)(function(mutation, state) {
+    ;(options.subscriber || subscriber)(store)(function(mutation, state) {
       if ((options.filter || filter)(mutation)) {
-        (options.setState || setState)(
+        ;(options.setState || setState)(
           key,
           (options.reducer || reducer)(state, options.paths || []),
           storage
-        );
+        )
       }
-    });
-  };
-};
+    })
+  }
+}
