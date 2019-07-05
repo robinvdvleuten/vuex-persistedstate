bc0bfd1a614e59797416693656f3a89cf9b7d2d5
Nelson Liu <nelsliu9121@gmail.com>
2019-07-05 09:08:43 +0800

    Revert "🎨 Fix prettier settings"
    This reverts commit 063fbd451e1382feb46e384dee18b846153955c6.

--- index.js
+++ index.js
@@ -2,85 +2,75 @@ import merge from 'deepmerge';
 import * as shvl from 'shvl';
 
 export default function(options, storage, key) {
-    options = options || {};
-    storage = options.storage || (window && window.localStorage);
-    key = options.key || 'vuex';
+  options = options || {};
+  storage = options.storage || (window && window.localStorage);
+  key = options.key || 'vuex';
 
-    function canWriteStorage(storage) {
-        try {
-            storage.setItem('@@', 1);
-            storage.removeItem('@@');
-            return true;
-        } catch (e) {}
+  function canWriteStorage(storage) {
+    try {
+      storage.setItem('@@', 1);
+      storage.removeItem('@@');
+      return true;
+    } catch (e) {}
 
-        return false;
-    }
+    return false;
+  }
 
-    function getState(key, storage, value) {
-        try {
-            return (value = storage.getItem(key)) &&
-                typeof value !== 'undefined'
-                ? JSON.parse(value)
-                : undefined;
-        } catch (err) {}
+  function getState(key, storage, value) {
+    try {
+      return (value = storage.getItem(key)) && typeof value !== 'undefined'
+        ? JSON.parse(value)
+        : undefined;
+    } catch (err) {}
 
-        return undefined;
-    }
+    return undefined;
+  }
 
-    function filter() {
-        return true;
-    }
+  function filter() {
+    return true;
+  }
 
-    function setState(key, state, storage) {
-        return storage.setItem(key, JSON.stringify(state));
-    }
+  function setState(key, state, storage) {
+    return storage.setItem(key, JSON.stringify(state));
+  }
 
-    function reducer(state, paths) {
-        return paths.length === 0
-            ? state
-            : paths.reduce(function(substate, path) {
-                  return shvl.set(substate, path, shvl.get(state, path));
-              }, {});
-    }
+  function reducer(state, paths) {
+    return paths.length === 0
+      ? state
+      : paths.reduce(function(substate, path) {
+          return shvl.set(substate, path, shvl.get(state, path));
+        }, {});
+  }
 
-    function subscriber(store) {
-        return function(handler) {
-            return store.subscribe(handler);
-        };
-    }
-
-    if (!canWriteStorage(storage)) {
-        throw new Error('Invalid storage instance given');
-    }
-
-    return function(store) {
-        const savedState = shvl.get(options, 'getState', getState)(
-            key,
-            storage
-        );
-
-        if (typeof savedState === 'object' && savedState !== null) {
-            store.replaceState(
-                merge(store.state, savedState, {
-                    arrayMerge:
-                        options.arrayMerger ||
-                        function(store, saved) {
-                            return saved;
-                        },
-                    clone: false,
-                })
-            );
-            (options.rehydrated || function() {})(store);
-        }
-
-        (options.subscriber || subscriber)(store)(function(mutation, state) {
-            if ((options.filter || filter)(mutation)) {
-                (options.setState || setState)(
-                    key,
-                    (options.reducer || reducer)(state, options.paths || []),
-                    storage
-                );
-            }
-        });
+  function subscriber(store) {
+    return function(handler) {
+      return store.subscribe(handler);
     };
-}
+  }
+
+  if (!canWriteStorage(storage)) {
+    throw new Error('Invalid storage instance given');
+  }
+
+  return function(store) {
+    const savedState = shvl.get(options, 'getState', getState)(key, storage);
+
+    if (typeof savedState === 'object' && savedState !== null) {
+      store.replaceState(merge(store.state, savedState, {
+        arrayMerge: options.arrayMerger || function (store, saved) { return saved },
+        clone: false,
+      }));
+      (options.rehydrated || function() {})(store);
+    }
+
+    (options.subscriber || subscriber)(store)(function(mutation, state) {
+      if ((options.filter || filter)(mutation)) {
+        (options.setState || setState)(
+          key,
+          (options.reducer || reducer)(state, options.paths || []),
+          storage
+        );
+      }
+    });
+  };
+};
--- test.js
+++ test.js
@@ -9,298 +9,293 @@ Vue.config.productionTip = false;
 Vue.use(Vuex);
 
 it('can be created with the default options', () => {
-    window.localStorage = new Storage();
-    expect(() => createPersistedState()).not.toThrow();
+  window.localStorage = new Storage();
+  expect(() => createPersistedState()).not.toThrow();
 });
 
 it("replaces store's state and subscribes to changes when initializing", () => {
-    const storage = new Storage();
-    storage.setItem('vuex', JSON.stringify({ persisted: 'json' }));
+  const storage = new Storage();
+  storage.setItem('vuex', JSON.stringify({ persisted: 'json' }));
 
-    const store = new Vuex.Store({ state: { original: 'state' } });
-    store.replaceState = jest.fn();
-    store.subscribe = jest.fn();
+  const store = new Vuex.Store({ state: { original: 'state' } });
+  store.replaceState = jest.fn();
+  store.subscribe = jest.fn();
 
-    const plugin = createPersistedState({ storage });
-    plugin(store);
+  const plugin = createPersistedState({ storage });
+  plugin(store);
 
-    expect(store.replaceState).toBeCalledWith({
-        original: 'state',
-        persisted: 'json',
-    });
-    expect(store.subscribe).toBeCalled();
+  expect(store.replaceState).toBeCalledWith({
+    original: 'state',
+    persisted: 'json'
+  });
+  expect(store.subscribe).toBeCalled();
 });
 
 it("does not replaces store's state when receiving invalid JSON", () => {
-    const storage = new Storage();
-    storage.setItem('vuex', '<invalid JSON>');
+  const storage = new Storage();
+  storage.setItem('vuex', '<invalid JSON>');
 
-    const store = new Vuex.Store({ state: { nested: { original: 'state' } } });
-    store.replaceState = jest.fn();
-    store.subscribe = jest.fn();
+  const store = new Vuex.Store({ state: { nested: { original: 'state' } } });
+  store.replaceState = jest.fn();
+  store.subscribe = jest.fn();
 
-    const plugin = createPersistedState({ storage });
-    plugin(store);
+  const plugin = createPersistedState({ storage });
+  plugin(store);
 
-    expect(store.replaceState).not.toBeCalled();
-    expect(store.subscribe).toBeCalled();
+  expect(store.replaceState).not.toBeCalled();
+  expect(store.subscribe).toBeCalled();
 });
 
 it("does not replaces store's state when receiving null", () => {
-    const storage = new Storage();
-    storage.setItem('vuex', JSON.stringify(null));
+  const storage = new Storage();
+  storage.setItem('vuex', JSON.stringify(null));
 
-    const store = new Vuex.Store({ state: { nested: { original: 'state' } } });
-    store.replaceState = jest.fn();
-    store.subscribe = jest.fn();
+  const store = new Vuex.Store({ state: { nested: { original: 'state' } } });
+  store.replaceState = jest.fn();
+  store.subscribe = jest.fn();
 
-    const plugin = createPersistedState({ storage });
-    plugin(store);
+  const plugin = createPersistedState({ storage });
+  plugin(store);
 
-    expect(store.replaceState).not.toBeCalled();
-    expect(store.subscribe).toBeCalled();
+  expect(store.replaceState).not.toBeCalled();
+  expect(store.subscribe).toBeCalled();
 });
 
 it("respects nested values when it replaces store's state on initializing", () => {
-    const storage = new Storage();
-    storage.setItem('vuex', JSON.stringify({ persisted: 'json' }));
+  const storage = new Storage();
+  storage.setItem('vuex', JSON.stringify({ persisted: 'json' }));
 
-    const store = new Vuex.Store({ state: { original: 'state' } });
-    store.replaceState = jest.fn();
-    store.subscribe = jest.fn();
+  const store = new Vuex.Store({ state: { original: 'state' } });
+  store.replaceState = jest.fn();
+  store.subscribe = jest.fn();
 
-    const plugin = createPersistedState({ storage });
-    plugin(store);
+  const plugin = createPersistedState({ storage });
+  plugin(store);
 
-    expect(store.replaceState).toBeCalledWith({
-        original: 'state',
-        persisted: 'json',
-    });
-    expect(store.subscribe).toBeCalled();
+  expect(store.replaceState).toBeCalledWith({
+    original: 'state',
+    persisted: 'json'
+  });
+  expect(store.subscribe).toBeCalled();
 });
 
 it('should persist the changed parial state back to serialized JSON', () => {
-    const storage = new Storage();
-    const store = new Vuex.Store({ state: {} });
+  const storage = new Storage();
+  const store = new Vuex.Store({ state: {} });
 
-    const plugin = createPersistedState({ storage, paths: ['changed'] });
-    plugin(store);
+  const plugin = createPersistedState({ storage, paths: ['changed'] });
+  plugin(store);
 
-    store._subscribers[0]('mutation', { changed: 'state' });
+  store._subscribers[0]('mutation', { changed: 'state' });
 
-    expect(storage.getItem('vuex')).toBe(JSON.stringify({ changed: 'state' }));
+  expect(storage.getItem('vuex')).toBe(JSON.stringify({ changed: 'state' }));
 });
 
 it('persist the changed partial state back to serialized JSON under a configured key', () => {
-    const storage = new Storage();
-    const store = new Vuex.Store({ state: {} });
+  const storage = new Storage();
+  const store = new Vuex.Store({ state: {} });
 
-    const plugin = createPersistedState({
-        storage,
-        key: 'custom',
-        paths: ['changed'],
-    });
-    plugin(store);
+  const plugin = createPersistedState({
+    storage,
+    key: 'custom',
+    paths: ['changed']
+  });
+  plugin(store);
 
-    store._subscribers[0]('mutation', { changed: 'state' });
+  store._subscribers[0]('mutation', { changed: 'state' });
 
-    expect(storage.getItem('custom')).toBe(
-        JSON.stringify({ changed: 'state' })
-    );
+  expect(storage.getItem('custom')).toBe(JSON.stringify({ changed: 'state' }));
 });
 
 it('persist the changed full state back to serialized JSON when no paths are given', () => {
-    const storage = new Storage();
-    const store = new Vuex.Store({ state: {} });
+  const storage = new Storage();
+  const store = new Vuex.Store({ state: {} });
 
-    const plugin = createPersistedState({ storage });
-    plugin(store);
+  const plugin = createPersistedState({ storage });
+  plugin(store);
 
-    store._subscribers[0]('mutation', { changed: 'state' });
+  store._subscribers[0]('mutation', { changed: 'state' });
 
-    expect(storage.getItem('vuex')).toBe(JSON.stringify({ changed: 'state' }));
+  expect(storage.getItem('vuex')).toBe(JSON.stringify({ changed: 'state' }));
 });
 
 it('persist the changed partial state back to serialized JSON under a nested path', () => {
-    const storage = new Storage();
-    const store = new Vuex.Store({ state: {} });
+  const storage = new Storage();
+  const store = new Vuex.Store({ state: {} });
 
-    const plugin = createPersistedState({
-        storage,
-        paths: ['foo.bar', 'bar'],
-    });
-    plugin(store);
+  const plugin = createPersistedState({
+    storage,
+    paths: ['foo.bar', 'bar']
+  });
+  plugin(store);
 
-    store._subscribers[0]('mutation', { foo: { bar: 'baz' }, bar: 'baz' });
+  store._subscribers[0]('mutation', { foo: { bar: 'baz' }, bar: 'baz' });
 
-    expect(storage.getItem('vuex')).toBe(
-        JSON.stringify({ foo: { bar: 'baz' }, bar: 'baz' })
-    );
+  expect(storage.getItem('vuex')).toBe(
+    JSON.stringify({ foo: { bar: 'baz' }, bar: 'baz' })
+  );
 });
 
 it('should not persist null values', () => {
-    const storage = new Storage();
-    const store = new Vuex.Store({
-        state: { alpha: { name: null, bravo: { name: null } } },
-    });
+  const storage = new Storage();
+  const store = new Vuex.Store({
+    state: { alpha: { name: null, bravo: { name: null } } }
+  });
 
-    const plugin = createPersistedState({
-        storage,
-        paths: ['alpha.name', 'alpha.bravo.name'],
-    });
+  const plugin = createPersistedState({
+    storage,
+    paths: ['alpha.name', 'alpha.bravo.name']
+  });
 
-    plugin(store);
+  plugin(store);
 
-    store._subscribers[0]('mutation', { charlie: { name: 'charlie' } });
+  store._subscribers[0]('mutation', { charlie: { name: 'charlie' } });
 
-    expect(storage.getItem('vuex')).toBe(
-        JSON.stringify({ alpha: { bravo: {} } })
-    );
+  expect(storage.getItem('vuex')).toBe(
+    JSON.stringify({ alpha: { bravo: {} } })
+  );
 });
 
 it('should not merge array values when rehydrating by default', () => {
-    const storage = new Storage();
-    storage.setItem('vuex', JSON.stringify({ persisted: ['json'] }));
+  const storage = new Storage();
+  storage.setItem('vuex', JSON.stringify({ persisted: ['json'] }));
 
-    const store = new Vuex.Store({ state: { persisted: ['state'] } });
-    store.replaceState = jest.fn();
-    store.subscribe = jest.fn();
+  const store = new Vuex.Store({ state: { persisted: ['state'] } });
+  store.replaceState = jest.fn();
+  store.subscribe = jest.fn();
 
-    const plugin = createPersistedState({ storage });
-    plugin(store);
+  const plugin = createPersistedState({ storage });
+  plugin(store);
 
-    expect(store.replaceState).toBeCalledWith({
-        persisted: ['json'],
-    });
+  expect(store.replaceState).toBeCalledWith({
+    persisted: ['json'],
+  });
 
-    expect(store.subscribe).toBeCalled();
+  expect(store.subscribe).toBeCalled();
 });
 
 it('should not clone circular objects when rehydrating', () => {
-    const circular = { foo: 'bar' };
-    circular.foo = circular;
+  const circular = { foo: 'bar' };
+  circular.foo = circular;
 
-    const storage = new Storage();
-    storage.setItem('vuex', JSON.stringify({ persisted: 'baz' }));
+  const storage = new Storage();
+  storage.setItem('vuex', JSON.stringify({ persisted: 'baz' }));
 
-    const store = new Vuex.Store({ state: { circular } });
-    store.replaceState = jest.fn();
-    store.subscribe = jest.fn();
+  const store = new Vuex.Store({ state: { circular } });
+  store.replaceState = jest.fn();
+  store.subscribe = jest.fn();
 
-    const plugin = createPersistedState({ storage });
-    plugin(store);
+  const plugin = createPersistedState({ storage });
+  plugin(store);
 
-    expect(store.replaceState).toBeCalledWith({
-        circular,
-        persisted: 'baz',
-    });
+  expect(store.replaceState).toBeCalledWith({
+    circular,
+    persisted: 'baz',
+  });
 
-    expect(store.subscribe).toBeCalled();
+  expect(store.subscribe).toBeCalled();
 });
 
 it('should apply a custom arrayMerger function', () => {
-    const storage = new Storage();
-    storage.setItem('vuex', JSON.stringify({ persisted: [1, 2] }));
+  const storage = new Storage();
+  storage.setItem('vuex', JSON.stringify({ persisted: [1, 2] }));
 
-    const store = new Vuex.Store({ state: { persisted: [1, 2, 3] } });
-    store.replaceState = jest.fn();
-    store.subscribe = jest.fn();
+  const store = new Vuex.Store({ state: { persisted: [1, 2, 3] } });
+  store.replaceState = jest.fn();
+  store.subscribe = jest.fn();
 
-    const plugin = createPersistedState({
-        storage,
-        arrayMerger: function(store, saved) {
-            return ['hello!'];
-        },
-    });
-    plugin(store);
+  const plugin = createPersistedState({ 
+    storage,
+    arrayMerger: function (store, saved) { return ['hello!'] },
+   });
+  plugin(store);
 
-    expect(store.replaceState).toBeCalledWith({
-        persisted: ['hello!'],
-    });
+  expect(store.replaceState).toBeCalledWith({
+    persisted: ['hello!'],
+  });
 
-    expect(store.subscribe).toBeCalled();
+  expect(store.subscribe).toBeCalled();
 });
 
 it("rehydrates store's state through the configured getter", () => {
-    const storage = new Storage();
+  const storage = new Storage();
 
-    const store = new Vuex.Store({ state: {} });
-    store.replaceState = jest.fn();
+  const store = new Vuex.Store({ state: {} });
+  store.replaceState = jest.fn();
 
-    const plugin = createPersistedState({
-        storage,
-        getState: () => ({ getter: 'item' }),
-    });
-    plugin(store);
+  const plugin = createPersistedState({
+    storage,
+    getState: () => ({ getter: 'item' })
+  });
+  plugin(store);
 
-    expect(store.replaceState).toBeCalledWith({ getter: 'item' });
+  expect(store.replaceState).toBeCalledWith({ getter: 'item' });
 });
 
 it('persist the changed state back through the configured setter', () => {
-    expect.assertions(1);
+  expect.assertions(1);
 
-    const storage = new Storage();
-    const store = new Vuex.Store({ state: {} });
+  const storage = new Storage();
+  const store = new Vuex.Store({ state: {} });
 
-    const plugin = createPersistedState({
-        storage,
-        setState: (key, state) => {
-            expect(state).toEqual({ setter: 'item' });
-        },
-    });
+  const plugin = createPersistedState({
+    storage,
+    setState: (key, state) => {
+      expect(state).toEqual({ setter: 'item' });
+    }
+  });
 
-    plugin(store);
+  plugin(store);
 
-    store._subscribers[0]('mutation', { setter: 'item' });
+  store._subscribers[0]('mutation', { setter: 'item' });
 });
 
 it('uses the configured reducer when persisting the state', () => {
-    const storage = new Storage();
-    const store = new Vuex.Store({ state: {} });
+  const storage = new Storage();
+  const store = new Vuex.Store({ state: {} });
 
-    const customReducer = jest.fn();
+  const customReducer = jest.fn();
 
-    const plugin = createPersistedState({
-        storage,
-        paths: ['custom'],
-        reducer: customReducer,
-    });
-    plugin(store);
+  const plugin = createPersistedState({
+    storage,
+    paths: ['custom'],
+    reducer: customReducer
+  });
+  plugin(store);
 
-    store._subscribers[0]('mutation', { custom: 'value' });
+  store._subscribers[0]('mutation', { custom: 'value' });
 
-    expect(customReducer).toBeCalledWith({ custom: 'value' }, ['custom']);
+  expect(customReducer).toBeCalledWith({ custom: 'value' }, ['custom']);
 });
 
 it('filters to specific mutations', () => {
-    const storage = new Storage();
-    const store = new Vuex.Store({ state: {} });
+  const storage = new Storage();
+  const store = new Vuex.Store({ state: {} });
 
-    const plugin = createPersistedState({
-        storage,
-        filter: mutation => ['filter'].indexOf(mutation) !== -1,
-    });
-    plugin(store);
+  const plugin = createPersistedState({
+    storage,
+    filter: mutation => ['filter'].indexOf(mutation) !== -1
+  });
+  plugin(store);
 
-    store._subscribers[0]('mutation', { changed: 'state' });
+  store._subscribers[0]('mutation', { changed: 'state' });
 
-    expect(storage.getItem('vuex')).toBeNull();
+  expect(storage.getItem('vuex')).toBeNull();
 
-    store._subscribers[0]('filter', { changed: 'state' });
+  store._subscribers[0]('filter', { changed: 'state' });
 
-    expect(storage.getItem('vuex')).toBe(JSON.stringify({ changed: 'state' }));
+  expect(storage.getItem('vuex')).toBe(JSON.stringify({ changed: 'state' }));
 });
 
 it('should call rehydrated callback once the state is replaced', () => {
-    const storage = new Storage();
-    storage.setItem('vuex', JSON.stringify({ persisted: 'json' }));
+  const storage = new Storage();
+  storage.setItem('vuex', JSON.stringify({ persisted: 'json' }));
 
-    const store = new Vuex.Store({ state: { original: 'state' } });
+  const store = new Vuex.Store({ state: { original: 'state' } });
+  const rehydrated = jest.fn();
 
-    const rehydrated = jest.fn();
+  const plugin = createPersistedState({ storage, rehydrated });
+  plugin(store);
 
-    const plugin = createPersistedState({ storage, rehydrated });
-    plugin(store);
-
-    expect(rehydrated).toBeCalledWith(store);
+  expect(rehydrated).toBeCalledWith(store);
 });
