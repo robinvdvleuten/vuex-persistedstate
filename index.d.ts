4692ca69ab82a6e7148ceb37f2dfe23b1a605c87
Robin van der Vleuten <robin@webstronauts.co>
2020-02-06 10:38:45 +0100

    Add overwrite option to typings

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,24 @@
-import { Store, MutationPayload } from 'vuex';
+import { Store, MutationPayload } from "vuex";
 
 interface Storage {
-    getItem: (key: string) => any;
-    setItem: (key: string, value: any) => void;
-    removeItem: (key: string) => void;
+  getItem: (key: string) => any;
+  setItem: (key: string, value: any) => void;
+  removeItem: (key: string) => void;
 }
 
 interface Options {
-    key?: string;
-    paths?: string[];
-    reducer?: (state: any, paths: string[]) => object;
-    subscriber?: (store: typeof Store) => (handler: () => void) => void;
-    storage?: Storage;
-    getState?: (key: string, storage: Storage) => any;
-    setState?: (key: string, state: typeof Store, storage: Storage) => void;
-    filter?: (mutation: MutationPayload) => boolean;
-    arrayMerger?: (state: any, saved: any) => any;
-    rehydrated?: (store: typeof Store) => void;
-    fetchBeforeUse?: boolean; 
+  key?: string;
+  paths?: string[];
+  reducer?: (state: any, paths: string[]) => object;
+  subscriber?: (store: typeof Store) => (handler: () => void) => void;
+  storage?: Storage;
+  getState?: (key: string, storage: Storage) => any;
+  setState?: (key: string, state: typeof Store, storage: Storage) => void;
+  filter?: (mutation: MutationPayload) => boolean;
+  arrayMerger?: (state: any, saved: any) => any;
+  rehydrated?: (store: typeof Store) => void;
+  fetchBeforeUse?: boolean;
+  overwrite?: boolean;
 }
 
 export default function createPersistedState(options?: Options): any;
