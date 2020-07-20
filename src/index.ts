629a70ee2c366df4b73add6e4d9db81b89830bc9
lopugit <ireply@myleisure.com.au>
2020-07-20 19:28:27 +1000

    Fixed options.merge type signature

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ interface Options<State> {
   fetchBeforeUse?: boolean;
   overwrite?: boolean;
   assertStorage?: (storage: Storage) => void | Error;
-	merge?: (object1: Object | Array, object2: Object | Array, options: Object) => object | any[];
+	merge?: (object1: Object | Array<any>, object2: Object | Array<any>, options: Object) => object | Array<any>;
 }
 
 export default function <State>(
